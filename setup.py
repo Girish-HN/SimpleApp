bc383abb7d00f0ef5c6af87de6fe5bea44c8e784
Girish HN <girishhnbharadwaj@gmail.com>
2021-07-06 20:00:37 +0530

    uploading tox file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = 'my_package',
+	version = '0.0.1',
+	description = "Testing the package setup",
+	author = 'Girish HN',
+	packages = find_packages(),
+	license = 'MIT'
+)
