ffcbb300b532c932df5456eea57edb6673bb00fb
Girish HN <girishhnbharadwaj@gmail.com>
2021-07-29 17:39:29 +0530

    github action

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-	name = 'my_package',
-	version = '0.0.1',
-	description = "Testing the package setup",
-	author = 'Girish HN',
-	packages = find_packages(),
-	license = 'MIT'
+    name="src",
+    version="0.0.1",
+    description="its a wine Q package", 
+    author="c17hawke", 
+    packages=find_packages(),
+    license="MIT"
 )
+
